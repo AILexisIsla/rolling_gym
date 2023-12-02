b818d2938e79bee3b391054dc794ae3c2e778612
LucasCorbalan <lucas.corbalan.23@gmail.com>
2023-12-02 09:04:22 -0300

    make a join with reasonsAndPlants branch and add changes for the visual

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
- reasonsAndPlans
-import Hero from "./components/Hero/Hero";
-import Plans from "./components/Plans/Plans";
-import Programs from "./components/Programs/Programs";
-import Reasons from "./components/Reasons/Reasons";
-
 import Login from "./components/views/Login/Login";
 import Register from "./components/views/Register/Register";
 import Home from "./components/views/Home/Home";
@@ -13,11 +7,6 @@ import Home from "./components/views/Home/Home";
 function App() {
   return (
     <div className="App">
-      <Hero />
-      <Programs />
-      <Reasons />
-      <Plans />
-
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Programs from "../../Programs/Programs";
 import Hero from "../../Hero/Hero";
+import Reasons from "../../Reasons/Reasons";
+import Plans from "../../Plans/Plans";
 
 const Home = () => {
   return (
     <div>
-      <Hero/>
-      <Programs/>
+      <Hero />
+      <Programs />
+      <Reasons />
+      <Plans />
     </div>
   );
 };
