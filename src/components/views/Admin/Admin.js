67ba686174bad0676bc0f97142f614b946168e45
LucasCorbalan <lucas.corbalan.23@gmail.com>
2023-12-04 16:55:40 -0300

    add change in classes

--- src/components/views/Admin/Admin.js
+++ src/components/views/Admin/Admin.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { Container, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./Admin.css";
@@ -6,13 +5,16 @@ import "./Admin.css";
 const Admin = ({ classes }) => {
   return (
     <div>
-      <Container className="py-5">
-        <h1>Clases</h1>
-        <Link className="btn" to="/CreateClass">
-          Crear Clase
-        </Link>
+      <Container className="container-admin">
+        <div>
+          <h1>Clases</h1>
+          <hr />
+          <Link className="btn" to="/CreateClass">
+            Crear Clase
+          </Link>
+        </div>
         {classes?.length !== 0 ? (
-          <Table bordered hover responsive className="align-middle mx-3">
+          <Table bordered hover responsive className="table-admin">
             <thead>
               <tr>
                 <th>#</th>
@@ -20,16 +22,28 @@ const Admin = ({ classes }) => {
                 <th>Details</th>
                 <th>Date</th>
                 <th>Time</th>
+                <th>Acciones</th>
               </tr>
             </thead>
             <tbody>
               {classes?.map((classItem, index) => (
                 <tr key={classItem?._id}>
                   <td>{index + 1}</td>
-                  <td>{classItem.nameClass}</td>
-                  <td>{classItem.detailsClass}</td>
-                  <td>{classItem.dateClass}</td>
-                  <td>{classItem.timeClass}</td>
+                  <td>{classItem?.nameClass}</td>
+                  <td>{classItem?.detailsClass}</td>
+                  <td>{classItem?.dateClass}</td>
+                  <td>{classItem?.timeClass}</td>
+                  <td className="w-25">
+                    <div className=" hero-buttons">
+                      <Link
+                        to={`/class/edit/${classes?._id}`}
+                        className="btn-edit-delete"
+                      >
+                        Editar
+                      </Link>
+                      <Link className="btn-edit-delete">Eliminar</Link>
+                    </div>
+                  </td>
                 </tr>
               ))}
             </tbody>
