bbd2171b58adc0fa4e733238143ac3020d4fa76f
LucasCorbalan <lucas.corbalan.23@gmail.com>
2023-12-05 17:49:36 -0300

    add createregister

--- src/components/views/Register/RegisterForm.js
+++ src/components/views/Register/RegisterForm.js
@@ -1,21 +1,24 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { Alert } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
 const RegisterForm = () => {
-  const [user, SetUser] = useState({});
+  const [user, SetUser] = useState({
+    nameUser: "",
+    telefono: "",
+    emailUser: "",
+    passwordUser: "",
+  });
   const [error, setError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
-  const URL = process.env.REACT_APP_API_USER;
+  const URL = process.env.REACT_APP_GYMNASIO_ROLLING_USER;
 
   const handleChange = (event) => {
-    const name = event.target.name; // Utiliza el atributo 'name' del campo de entrada
+    const name = event.target.name;
     const value = event.target.value;
     SetUser((values) => ({ ...values, [name]: value }));
   };
-
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -30,26 +33,22 @@ const RegisterForm = () => {
     };
 
     try {
-      const res = await axios.post(`${URL}`, newUser);
-      console.log(res);
+      const res = await axios.post(`${URL}/Register`, newUser);
 
       if (res && res.data) {
         if (res.status === 201) {
           Swal.fire("Created!", "Your user has been created.", "success");
           const data = res.data;
           console.log(data);
-          // Realizar las acciones necesarias después de un registro exitoso
-          // Por ejemplo, almacenar datos en el local storage, establecer el usuario autenticado, etc.
-          // ...
+          localStorage.setItem("user-token", JSON.stringify(data));
+          navigate("/Login");
         }
       } else {
         console.error("Error: Response or response data is undefined");
       }
     } catch (error) {
-      console.log(error);
-      setError(true);
-      error.response?.data?.message &&
-        setErrorMessage(error.response.data.message);
+      console.error("Error al registrar el usuario", error.response.data);
+      setError(error.response.data.message);
     }
   };
 
@@ -88,23 +87,20 @@ const RegisterForm = () => {
           value={user.passwordUser || ""}
           onChange={(e) => handleChange(e)}
         ></input>
-
+        {error && <p className="error-message">{error}</p>}
         <div className="sign-in-button">
           <button type="submit" className="btn sign-in">
             REGISTER
           </button>
           <p>
             <span>
-              <Link to='/Login' className="sign-up">| Sign IN |</Link>
+              <Link to="/Login" className="sign-up">
+                | Sign IN |
+              </Link>
             </span>
           </p>
         </div>
       </form>
-      {error ? (
-        <Alert variant="danger" onClick={() => setError(false)} dismissible>
-          {errorMessage}
-        </Alert>
-      ) : null}
     </div>
   );
 };
