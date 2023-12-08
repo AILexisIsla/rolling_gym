b5d7184a0262394cbd574218229df75d7301524c
LucasCorbalan <lucas.corbalan.23@gmail.com>
2023-12-08 14:24:48 -0300

    add change weather

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,24 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { FaThermometerFull } from "react-icons/fa";
+import { FaThermometerEmpty } from "react-icons/fa";
+import { FaThermometerHalf } from "react-icons/fa";
 
 function Weatherapi() {
-  const [datos, setDatos] = useState(null);
-
+  const [datos, setDatos] = useState(false);
+  const kelvin = 273.15;
   useEffect(() => {
-    axios.get('http://api.weatherapi.com/')
-      .then(response => {
-        setDatos(response.data);
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/weather?q=tucuman&appid=5f275a2b80c7aabd2aaae0a48456e599"
+      )
+      .then((response) => {
+        const temp = Math.floor(response.data.main.temp);
+        const temperatura = Math.floor(temp - kelvin);
+        setDatos(temperatura);
       });
   }, []);
-
+  const getIconoPorTemperatura = (temperatura) => {
+    if (temperatura < 10) {
+      return <FaThermometerEmpty style={{ color: "green", fontSize:'25px' }}/>;
+    } else if (temperatura >= 10 && temperatura < 20) {
+      return <FaThermometerHalf style={{ color: "lightblue", fontSize:'25px' }}/>;
+    } else {
+      return <FaThermometerFull style={{ color: "red", fontSize:'25px' }} />;
+    }
+  };
   return (
     <div>
-      {datos ? (
-        Object.keys(datos).map((key, i) => (
-          <div key={i}>
-            <strong>{key}:</strong> {datos[key]}
-          </div>
-        ))
+      {datos !== false ? (
+        <p className="datos-temp">
+          {getIconoPorTemperatura(datos)} {datos}°C
+        </p>
       ) : (
         <p>Cargando...</p>
       )}
