36a5d315fa49246b8fff56b3d922673edfbac89b
AILexisIsla <alexis.u.isla@gmail.com>
2023-12-18 02:35:03 -0300

    animation error 404

--- src/components/views/Error404/Error404.jsx
+++ src/components/views/Error404/Error404.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import "../Error404/Error.css";
-import img1 from "../../../assets/img1.svg.png";
+
+import Lottie from "lottie-react"
+import animationData from "../../../assets/Animation - 1702872364939.json";
 
 const Error404 = () => (
-  <div className="container">
-    <img src={img1} alt="" />
-    <h1>La página que ha solicitado no se encuentra disponible</h1>
-    <div className="boton">
-    <Link to="/"><button>Volver atras</button></Link>
-    </div>
+  <div className="min-h-screen flex items-center justify-content-center">
+  <div className="text-center space-y-4">
+    <h1 className="text-2x1 font-bold">ERROR 404</h1>
+    <p>La página que ha solicitado no se encuentra disponible.</p>
   </div>
+  <div className="flex gap-2 justify-center"><Link to="/"><button>Volver atras</button></Link></div>
+  <div><Lottie className="lottie-animation" animationData={animationData}/></div>
+</div>
 );
 
 export default Error404;
